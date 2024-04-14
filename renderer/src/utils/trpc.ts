3610bc0cb435dfbbba7ff1d6a1a6fe2ea9e213fd
Yann Hippert <yann.hippert@hotmail.com>
2024-04-14 19:04:15 +0200

    chore: upgrade dependencies

--- renderer/src/utils/trpc.ts
+++ renderer/src/utils/trpc.ts
@@ -1,7 +1,4 @@
-import type { AppRouter } from "../../../api/router/index";
-import { createTRPCReact } from "@trpc/react-query";
-import type { GetInferenceHelpers } from "@trpc/server";
+import type { AppRouter } from '../../../api/router/index';
+import { createTRPCReact } from '@trpc/react-query';
 
 export const trpc = createTRPCReact<AppRouter>();
-
-export type InferProcedures = GetInferenceHelpers<AppRouter>;
